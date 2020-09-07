5008c44ee848465dfe633cae1111792a347c33bf
jhhall <jasun.hall@gmail>
2020-09-06 21:32:46 -0500

    update

--- project/login_unit_test.py
+++ project/login_unit_test.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 import unittest
 import pytest
 import settings
@@ -9,16 +10,25 @@ from scripts.pages import page
 WEB_DRIVER_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "webdrivers")
 sys.path.append(WEB_DRIVER_PATH)
 
-
 class LoginWorkFlow(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome(os.path.join(WEB_DRIVER_PATH, "chromedriver.exe"))
         self.driver.get("http://automationpractice.com")
 
-
-    
-
+    def test_log_in_with_valid_username_and_valid_password(self):
+        main_page = page.MainPage(self.driver)
+        assert main_page.is_title_matches_main_page()
+        main_page.click_signin_menu()
+        auth_page = page.AuthenticationPage(self.driver)
+        assert auth_page.is_title_matches_auth_page()
+        auth_page.emailAddressInputElement = settings.EMAIL_ADDRESS
+        auth_page.emailPasswordInputElement = settings.EMAIL_PASSWORD
+        auth_page.click_signin_button()
+        assert auth_page.is_signout_visible()
+        assert auth_page.is_user_account_visible()
+        auth_page.click_signout_button()
+        
 
     # def test_log_in_with_incorrect_username_and_incorrect_password(self):
     #     main_page = page.MainPage(self.driver)
@@ -50,9 +60,11 @@ class LoginWorkFlow(unittest.TestCase):
     #     assert auth_page.is_title_matches_auth_page()
     #     auth_page.emailAddressInputElement = "jasun.hall@gmail.com"
     #     auth_page.emailPasswordInputElement = "Ptest092020#".swapcase()
+
     #     auth_page.click_sign_in_button()
     #     assert 'Authentication failed.' == auth_page.get_banner_alert_message()
 
     def tearDown(self):
+        time.sleep(5)
         self.driver.close()
 
--- project/scripts/pages/page.py
+++ project/scripts/pages/page.py
@@ -25,10 +25,19 @@ class MainPage(BasePage):
     def is_title_matches_main_page(self):
         return "My Store" in self.driver.title
 
-    def click_sign_menu(self):
+    def click_signin_menu(self):
         element = self.driver.find_element(*MainPageLocators.SIGN_IN_MENU)
         element.click()
 
+    def click_signout_menu(self):
+        element = self.driver.find_element(*MainPageLocators.SIGN_OUT_MENU)
+        element.click()
+
+    def is_signout_visible(self):
+        if self.driver.find_element(*MainPageLocators.SIGN_OUT_MENU):
+            return True
+        else:
+            return False
 
 class AuthenticationPage(BasePage):
 
@@ -38,11 +47,27 @@ class AuthenticationPage(BasePage):
     def is_title_matches_auth_page(self):
         return "Login - My Store" in self.driver.title
 
+    def is_signout_visible(self):
+        if self.driver.find_element(*AuthenticationPageLocators.SIGN_OUT_MENU):
+            return True
+        else:
+            return False
+
+    def is_user_account_visible(self):
+        if self.driver.find_element(*AuthenticationPageLocators.SIGN_OUT_MENU):
+            return True
+        else:
+            return False
+
     def did_email_error_banner(self):
         return "An email address required." in self.driver.page_source
 
-    def click_sign_in_button(self):
+    def click_signin_button(self):
         element = self.driver.find_element(*AuthenticationPageLocators.SIGN_IN_BUTTON)
+        element.click()
+
+    def click_signout_button(self):
+        element = self.driver.find_element(*AuthenticationPageLocators.SIGN_OUT_MENU)
         element.click()
 
     def get_banner_alert_message(self):
